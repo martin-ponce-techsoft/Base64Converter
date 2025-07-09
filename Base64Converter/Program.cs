96525a794a02604b0840b73ca6cb61da860df61f
martin-ponce-techsoft <martinponce500@hotmail.com>
2025-07-09 16:50:35 -0700

    Add project files.

---
+++ Base64Converter/Program.cs
@@ -0,0 +1,28 @@
+﻿using Spectre.Console;
+
+while (true)
+{
+    var fruit = AnsiConsole.Prompt(
+    new SelectionPrompt<string>()
+        .Title("What's your [green]favorite fruit[/]?")
+        .PageSize(5)
+        .MoreChoicesText("[grey](Move up and down to reveal more fruits)[/]")
+        .AddChoices(new[] {
+            "Apple", "Apricot", "Avocado",
+            "Banana", "Blackcurrant", "Blueberry",
+            "Cherry", "Cloudberry", "Cocunut", "wola", "hola", "hello", "hej", "hallå",
+        }));
+    Console.WriteLine("Enter a string to encode in Base64:");
+    var value = Console.ReadLine();
+    if (!string.IsNullOrEmpty(value))
+    {
+        var bytes = System.Text.Encoding.UTF8.GetBytes(value);
+        var base64 = Convert.ToBase64String(bytes);
+        Console.WriteLine($"Base64 encoded: {base64}");
+    }
+    else
+    {
+        Console.WriteLine("No input provided.");
+    }
+    Console.WriteLine();
+}
---
+++ Base64ConverterApi/Controllers/Base64Controller.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Base64ConverterApi.Controllers
+{
+    [Route("api/base64")]
+    [ApiController]
+    public class Base64Controller : ControllerBase
+    {
+        [HttpGet("encode")]
+        public ActionResult<string> Encode(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return BadRequest("Input value cannot be null or empty.");
+            }
+            var bytes = System.Text.Encoding.UTF8.GetBytes(value);
+            var base64 = Convert.ToBase64String(bytes);
+            return Ok(base64);
+        }
+    }
+}
